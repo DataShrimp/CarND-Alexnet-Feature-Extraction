c64c6ef7d3d6517d2e1a108f8c27483bb1733fd1
zhaoxu <zhaoxu@zhaoxudeMacBook-Pro.local>
2017-10-17 20:13:11 +0800

    bug fix df

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -1,30 +1,35 @@
 import pickle
+import time
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
 from alexnet import AlexNet
 
 # TODO: Load traffic signs data.
-training_file = "../CarND-Traffic-Sign-Classifier-Project/traffic-signs-data/train.p"
-validation_file= "../CarND-Traffic-Sign-Classifier-Project/traffic-signs-data/valid.p"
-testing_file = "../CarND-Traffic-Sign-Classifier-Project/traffic-signs-data/test.p"
+with open('./train.p', 'rb') as f:
+	data = pickle.load(f)
+#training_file = "../CarND-Traffic-Sign-Classifier-Project/traffic-signs-data/train.p"
+#validation_file= "../CarND-Traffic-Sign-Classifier-Project/traffic-signs-data/valid.p"
+#testing_file = "../CarND-Traffic-Sign-Classifier-Project/traffic-signs-data/test.p"
 
-with open(training_file, mode='rb') as f:
-    train = pickle.load(f)
-with open(validation_file, mode='rb') as f:
-    valid = pickle.load(f)
-with open(testing_file, mode='rb') as f:
-    test = pickle.load(f)
+#with open(training_file, mode='rb') as f:
+#    train = pickle.load(f)
+#with open(validation_file, mode='rb') as f:
+#    valid = pickle.load(f)
+#with open(testing_file, mode='rb') as f:
+#    test = pickle.load(f)
 
 # TODO: Split data into training and validation sets.
-X_train, y_train = train['features'], train['labels']
-X_valid, y_valid = valid['features'], valid['labels']
-X_test, y_test = test['features'], test['labels']
+X_train, X_valid, y_train, y_valid = train_test_split(data['features'], data['labels'], test_size=0.33, random_state=0)
+#X_train, y_train = train['features'], train['labels']
+#X_valid, y_valid = valid['features'], valid['labels']
+#X_test, y_test = test['features'], test['labels']
 
 # TODO: Define placeholders and resize operation.
 nb_classes = 43
 x = tf.placeholder(tf.float32, (None, 32, 32, 3))
 y = tf.placeholder(tf.int64, shape=(None))
-y_onehot = tf.one_hot(y, nb_classes)
+#y_onehot = tf.one_hot(y, nb_classes)
 resize = tf.image.resize_images(x, [227, 227])
 
 # TODO: pass placeholder as first argument to `AlexNet`.
@@ -36,50 +41,54 @@ fc7 = tf.stop_gradient(fc7)
 
 # TODO: Add the final layer for traffic sign classification.
 shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
-fc8W = tf.Variable(tf.truncated_normal(shape, stddev=1e-3))
+fc8W = tf.Variable(tf.truncated_normal(shape, stddev=1e-2))
 fc8b = tf.Variable(tf.zeros(nb_classes))
-logits = tf.matmul(fc7, fc8W) + fc8b
-probs = tf.nn.softmax(logits)
+#logits = tf.matmul(fc7, fc8W) + fc8b
+logits = tf.nn.xw_plus_b(fc7, fc8W, fc8b)
+#probs = tf.nn.softmax(logits)
 
 # TODO: Define loss, training, accuracy operations.
 # HINT: Look back at your traffic signs project solution, you may
 # be able to reuse some the code.
-rate = 0.001
+#rate = 0.001
 EPOCHS = 10
 BATCH_SIZE = 128
 
 cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=logits, labels=y)
-loss = tf.reduce_mean(cross_entropy)
-opt = tf.train.AdamOptimizer(rate).minimize(loss, var_list=[fc8W, fc8b])
+loss_op = tf.reduce_mean(cross_entropy)
+opt = tf.train.AdamOptimizer()
+train_op = opt.minimize(loss_op, var_list=[fc8W, fc8b])
 
 correct_prediction = tf.arg_max(logits, 1)
 accuracy_operation = tf.reduce_mean(tf.cast(tf.equal(correct_prediction,y), tf.float32))
 
-def evaluate(X_data, y_data):
+def evaluate(X_data, y_data, sess):
     num_examples = len(X_data)
     total_accuracy = 0
-    sess = tf.get_default_session()
+    total_loss = 0
     for offset in range(0, num_examples, BATCH_SIZE):
         batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
-        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        loss, accuracy = sess.run([loss_op, accuracy_operation], feed_dict={x: batch_x, y: batch_y})
+        total_loss += (loss * len(batch_x))
         total_accuracy += (accuracy * len(batch_x))
-    return total_accuracy / num_examples
+    return total_loss / num_examples, total_accuracy / num_examples
 
 # TODO: Train and evaluate the feature extraction model.
-from sklearn.utils import shuffle
-
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     
     for i in range(EPOCHS):
         x_train, y_train = shuffle(X_train, y_train)
+        t0 = time.time()
         for offset in range(0, X_train.shape[0], BATCH_SIZE):
             end = offset + BATCH_SIZE
             batch_x, batch_y = x_train[offset:end], y_train[offset:end]
-            sess.run(opt, feed_dict={x: batch_x, y: batch_y})
+            sess.run(train_op, feed_dict={x: batch_x, y: batch_y})
             
-        valid = evaluate(X_valid, y_valid)
+        val_loss, val_acc = evaluate(X_valid, y_valid, sess)
         print("EPOCH {} ...".format(i))
-        print("Valid Accuracy: {:.3f}".format(valid))
+        print("Time: {:.3f} seconds".format(time.time()-t0))
+        print("Valid Loss: {:.3f}".format(val_loss))
+        print("Valid Accuracy: {:.3f}".format(val_acc))
         print()
 
